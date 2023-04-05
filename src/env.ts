8de2b2ab07d02fe995914a936d4c60ff1b53299f
YanLobat <yanlavryushev@gmail.com>
2023-04-05 21:07:42 +0300

    dropped some old shift

--- src/env.ts
+++
@@ -1,3 +0,0 @@
-// Single entry point of vars from .env (no destructuring)
-
-export const EXAMPLE = process.env.REACT_APP_EXAMPLE
--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import './index.css'
-
-import {render} from 'react-dom'
-
-import {TodoApp} from './todo-app'
-
-render(<TodoApp />, document.querySelector('#root'))
